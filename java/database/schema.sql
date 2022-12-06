1931b8253c9b79c3d190842de3dbf2ef7138a591
Sara McMullen <smcmullen87@gmail.com>
2022-12-06 10:51:01 -0500

    database work

--- java/database/schema.sql
+++ java/database/schema.sql
@@ -1,5 +1,9 @@
 BEGIN TRANSACTION;
 
+DROP TABLE IF EXISTS class_schedule;
+DROP TABLE IF EXISTS goals;
+DROP TABLE IF EXISTS equipment_usage;
+DROP TABLE IF EXISTS workout;
 DROP TABLE IF EXISTS users;
 
 CREATE TABLE users (
@@ -10,50 +14,56 @@ CREATE TABLE users (
 	email varchar(60) NOT NULL UNIQUE,
 	role varchar(50) NOT NULL,
 	image_file varchar(500) NOT NULL UNIQUE,
+	class_id integer,
 
-	CONSTRAINT PK_user PRIMARY KEY (user_id)
-);
-
-CREATE TABLE employee (
-    employee_id SERIAL,
-    user_id integer NOT NULL,
-
-    CONSTRAINT PK_employee PRIMARY KEY (employee_id),
-    CONSTRAINT FK_user FOREIGN KEY (user_id)
+	CONSTRAINT PK_user PRIMARY KEY (user_id),
+	CONSTRAINT FK_class_schedule FOREIGN KEY (class_id) references class_schedule
 );
 
 CREATE TABLE workout (
     workout_id SERIAL,
-    user_id NOT NULL,
-    check_in CURRENT_TIMESTAMP,
-    check_out CURRENT_TIMESTAMP,
+    user_id integer NOT NULL,
+    check_in timestamp default CURRENT_TIMESTAMP,
+    check_out timestamp default CURRENT_TIMESTAMP,
 
     CONSTRAINT PK_workouts PRIMARY KEY (workout_id),
-    CONSTRAINT FK_user FOREIGN KEY (user_id);
+    CONSTRAINT FK_user FOREIGN KEY (user_id) references users
 
 );
 
 CREATE TABLE equipment_usage (
     equipment_id SERIAL,
-    workout_id NOT NULL,
+    workout_id integer NOT NULL,
     exercise_type varchar(50) NOT NULL,
-    workout_date CURRENT_DATE,
+    workout_date timestamp default CURRENT_DATE,
     sets integer,
     reps integer,
     weight DECIMAL(5, 2),
 
     CONSTRAINT PK_equipment_usage PRIMARY KEY (equipment_id),
-    CONSTRAINT FK_workout FOREIGN KEY (workout_id),
+    CONSTRAINT FK_workout FOREIGN KEY (workout_id) references workout,
     CONSTRAINT weight_limit CHECK (weight >= 0)
 );
 
 CREATE TABLE goals (
     goal_id SERIAL,
-    user_id NOT NULL,
-    goal_type varchar(50) NOT NULL;
+    user_id integer NOT NULL,
+    goal_type varchar(50) NOT NULL,
 
     CONSTRAINT PK_goals PRIMARY KEY (goal_id),
-    CONSTRAINT FK_user FOREIGN KEY (user_id);
+    CONSTRAINT FK_user FOREIGN KEY (user_id) references users
+);
+
+CREATE TABLE class_schedule (
+    class_id SERIAL,
+    class_name varchar(50) NOT NULL,
+    class_instructor varchar(50) NOT NULL,
+    class_description varchar(200),
+    class_time time NOT NULL,
+    class_day varchar(10) NOT NULL,
+
+    CONSTRAINT PK_class_schedule PRIMARY KEY (class_id)
+
 );
 
 COMMIT TRANSACTION;
