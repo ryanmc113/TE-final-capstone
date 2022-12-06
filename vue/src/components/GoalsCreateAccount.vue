824748a858afd85264d468ee5778949aee7ba1e2
Ryan McUmber <ryanmma.rm@gmail.com>
2022-12-06 15:59:35 -0500

    employee Account

--- vue/src/components/GoalsCreateAccount.vue
+++ vue/src/components/GoalsCreateAccount.vue
@@ -1,4 +1,17 @@
 <template>
-    <div>
+    <div class = "goals">
+        <h1>Goals</h1>
+        <!-- <input type = "" name = "createGoals" v-model = "" -->
     </div>
 </template>
+
+<script>
+
+
+export default {
+    name: "goals-create-account",
+    data(){
+
+    }
+}
+</script>
---
+++ vue/src/views/EmployeeLogin.vue
@@ -0,0 +1,83 @@
+<template class>
+  <div id="login" class="text-center">
+    <form class="form-signin" @submit.prevent="login">
+      <h1 class="h3 mb-3 font-weight-normal is-size-2">Employee Log In</h1>
+      <div
+        class="alert alert-danger"
+        role="alert"
+        v-if="invalidCredentials"
+      >Invalid username and password!</div>
+      <!-- <div
+        class="alert alert-success"
+        role="alert"
+        v-if="this.$route.query.registration"
+      >Thank you for registering, please sign in.</div> -->
+      <div class = "field">
+      <label for="username" class="sr-only label">Username</label>
+      </div>
+      <input
+        type="text"
+        id="username"
+        class="form-control"
+        placeholder="Username"
+        v-model="user.username"
+        required
+        autofocus
+      />
+      <div class = "field">
+      <label for="password" class="sr-only label">Password</label>
+      </div>
+      <div class = "field">
+      <input
+        type="password"
+        id="password"
+        class="form-control"
+        placeholder="Password"
+        v-model="user.password"
+        required
+      />
+      </div>
+      <div class = "field">
+        <button type="submit" class = "button is-primary">Sign in</button>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+import authService from "../services/AuthService";
+
+export default {
+  name: "employeeLogin",
+  components: {},
+  data() {
+    return {
+      user: {
+        username: "",
+        password: ""
+      },
+      invalidCredentials: false
+    };
+  },
+  methods: {
+    login() {
+      authService
+        .login(this.user)
+        .then(response => {
+          if (response.status == 200) {
+            this.$store.commit("SET_AUTH_TOKEN", response.data.token);
+            this.$store.commit("SET_USER", response.data.user);
+            this.$router.push("/employee");
+          }
+        })
+        .catch(error => {
+          const response = error.response;
+
+          if (response.status === 401) {
+            this.invalidCredentials = true;
+          }
+        });
+    }
+  }
+};
+</script>
