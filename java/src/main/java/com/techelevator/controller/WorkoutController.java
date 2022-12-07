096dc5e13d745b89e472928fbca506ac218ac523
Gabriel May <gmay8231@gmail.com>
2022-12-07 16:18:13 -0500

    created updateAccount method and added it to controller

--- java/src/main/java/com/techelevator/controller/WorkoutController.java
+++ java/src/main/java/com/techelevator/controller/WorkoutController.java
@@ -1,10 +1,17 @@
 package com.techelevator.controller;
 
+import com.techelevator.model.WorkoutMetrics;
+import org.springframework.web.bind.annotation.GetMapping;
+
 public class WorkoutController {
 
     //log workout (post to workout table, param will be the workoutlog object)
 
     //get list of logged workouts (to view metrics of logged workouts and visits returns a workoutmetrics log)
+    @GetMapping(path = "workouts")
+    public WorkoutMetrics getWorkoutHistory(int accountId) {
+        return null;
+    }
 
     //get exercise by id (will bring back exercise object for assistance button)
 
