10b020611999125a8239a9fb84efc8468fe028a1
David Sikes <dgostomi@gmail.com>
2022-12-13 15:51:05 -0500

    THIS WORKS PERFECTLY

--- vue/src/components/WorkoutHistory.vue
+++ vue/src/components/WorkoutHistory.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+Month year
     <table>
       <thead>
         <tr>
@@ -7,10 +8,11 @@
         </tr>
       </thead>
       <tbody>
-
-        <tr v-for="day in workoutDays" v-bind:key="day.visitId">
-          <router-link :to="{ name: 'usersWorkoutsThatDay', params: { id: day.visitId} }">
-            {{ day.checkIn }}</router-link
+        <tr v-for="day in fullWorkoutDays" v-bind:key="day.visitId">
+          <router-link
+            :to="{ name: 'usersWorkoutsThatDay', params: { id: day.visitId } }"
+          >
+          <div class="theMonth"> <div> {{day.month}}</div> &nbsp; <div>{{ day.day }}</div></div></router-link
           >
         </tr>
       </tbody>
@@ -24,19 +26,76 @@ import UserService from "../services/UserService.js";
 export default {
   data() {
     return {
-      
-      workoutDays: [],
+      year:[2022],
+      months:['January', 'February','March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+      currentMonth: '',
+      fullWorkoutDays: [],
+      makingADay: {
+        visitId: '',
+        year: '',
+        month: '',
+        day: '',
+      },
     };
   },
   created() {
     this.getDaysAtGym();
+    // this.getDate();
   },
   methods: {
     getDaysAtGym() {
       UserService.getVisitLog().then((response) => {
-        this.workoutDays = response.data;
+         response.data.forEach((workout) => {
+          this.makingADay.visitId = workout.visitId;
+          this.makingADay.year = workout.checkIn.substring(0, 4);
+          let thisMonth = workout.checkIn.substring(5, 7);
+          this.makingADay.month = this.getMonthName(thisMonth)
+          this.makingADay.day = workout.checkIn.substring(8, 10);
+          this.fullWorkoutDays.push(this.makingADay);
+          this.makingADay = {
+            visitId: '',
+            year: '',
+            month: '',
+            day: '',
+          };
+        });
       });
     },
+    getMonthName(month){
+if (month == 1){
+  month = 'January'
+} else if (month == 2){
+  month = 'February'
+}else if (month == 3){
+  month = 'March'
+}else if (month == 4){
+ return month = 'April'
+}else if (month == 5){
+  month = 'May'
+}else if (month == 6){
+  month = 'June'
+}else if (month == 7){
+  month = 'July'
+}else if (month == 8){
+  month = 'August'
+}else if (month == 9){
+  month = 'September'
+}else if (month == 10){
+  month = 'October'
+}else if (month == 11){
+  month = 'November'
+}else if (month == 12){
+  month = 'December'
+} return month
+    }
+    
   },
 };
 </script>
+
+<style scoped>
+.theMonth{
+  display: flex;
+
+}
+</style>
