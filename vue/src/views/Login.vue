c9cb42e03e641dd2c240be7efd89dbdc2c6b17a3
Sara McMullen <smcmullen87@gmail.com>
2022-12-14 14:07:08 -0500

    added pic to front page

--- vue/src/views/Login.vue
+++ vue/src/views/Login.vue
@@ -1,19 +1,23 @@
 <template class>
   <div id="login" class="text-center">
+    <section><img src="people_workout.png" /></section>
     <form class="form-signin" @submit.prevent="login">
-      <h1 class="h3 mb-3 font-weight-normal is-size-2">Welcome to the Workout Companion</h1>
-      <div
-        class="alert alert-danger"
-        role="alert"
-        v-if="invalidCredentials"
-      >Invalid username and password!</div>
+      <h1 class="h3 mb-3 font-weight-normal is-size-2">
+        Welcome to the Workout Companion
+      </h1>
+
+      <div class="alert alert-danger" role="alert" v-if="invalidCredentials">
+        Invalid username and password!
+      </div>
       <div
         class="alert alert-success"
         role="alert"
         v-if="this.$route.query.registration"
-      >Thank you for registering, please sign in.</div>
-      <div class = "field">
-      <label for="username" class=" label">Username</label>
+      >
+        Thank you for registering, please sign in.
+      </div>
+      <div class="field">
+        <label for="username" class="label">Username</label>
       </div>
       <input
         type="text"
@@ -24,23 +28,23 @@
         required
         autofocus
       />
-      <br>
-    
-      <div class = "field">
-      <label for="password" class="label">Password</label>
+      <br />
+
+      <div class="field">
+        <label for="password" class="label">Password</label>
       </div>
-      <div class = "field">
-      <input
-        type="password"
-        id="password"
-        class="form-control"
-        placeholder="Password"
-        v-model="user.password"
-        required
-      />
+      <div class="field">
+        <input
+          type="password"
+          id="password"
+          class="form-control"
+          placeholder="Password"
+          v-model="user.password"
+          required
+        />
       </div>
-      <div class = "field">
-        <button type="submit" class = "button is-primary">Sign in</button>
+      <div class="field">
+        <button type="submit" class="button is-primary">Sign in</button>
       </div>
     </form>
   </div>
@@ -56,57 +60,56 @@ export default {
     return {
       user: {
         username: "",
-        password: ""
+        password: "",
       },
-      invalidCredentials: false
+      invalidCredentials: false,
     };
   },
   methods: {
     login() {
       authService
         .login(this.user)
-        .then(response => {
+        .then((response) => {
           if (response.status == 200) {
             this.$store.commit("SET_AUTH_TOKEN", response.data.token);
             this.$store.commit("SET_USER", response.data.user);
             this.$router.push("/");
           }
         })
-        .catch(error => {
+        .catch((error) => {
           const response = error.response;
 
           if (response.status === 401) {
             this.invalidCredentials = true;
           }
         });
-    }
-  }
+    },
+  },
 };
 </script>
 
 <style>
-.text-center{
-  background-color:#A7BBC3 ;
-  border: 10px solid #012F63;
+.text-center {
+  background-color: #a7bbc3;
+  border: 10px solid #012f63;
   border-radius: 20px;
   max-width: 700px;
   margin: auto;
 
   height: 50vh;
   display: flex;
-  align-items:center;
+  align-items: center;
   justify-content: center;
 }
 
-.button{
-  background-color: #FE6678 !important;
+.button {
+  background-color: #fe6678 !important;
 }
 
-.form-signin{
+.form-signin {
   flex-direction: column;
   display: flex;
-  align-items:center;
+  align-items: center;
   justify-content: center;
-  
 }
 </style>
